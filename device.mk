41f290ea6f9a854b75e7127a82ff91b7a8be1982
Ahmed Shahin <ahmedshahinx0x@gmail.com>
2025-09-29 21:47:51 +0300

    lisbon: Import stock rootdir
    Signed-off-by: Ahmed Shahin  <ahmedshahinx0x@gmail.com>
    
    Change-Id: I22f34e7dff3a05a47e320b75336754d6b0b4cc06

--- device.mk
+++ device.mk
@@ -7,6 +7,59 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    pstore_annotate.sh \
+    hardware_revisions.sh \
+    init.mmi.usb.sh \
+    init.mmi.backup.trustlet.sh \
+    apanic_annotate.sh \
+    init.oem.fingerprint.overlay.sh \
+    init.mmi.block_perm.sh \
+    init.oem.fingerprint2.overlay.sh \
+    init.insmod.sh \
+    apanic_copy.sh \
+    apanic_save.sh \
+    init.mmi.boot.sh \
+    init.mmi.modules.sh \
+    init.oem.hw.sh \
+    apanic_mtk.sh \
+    init.mmi.touch.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.mt6785 \
+    meta_init.connectivity.rc \
+    init.project.rc \
+    meta_init.rc \
+    factory_init.rc \
+    init.connectivity.common.rc \
+    multi_init.rc \
+    init.modem.rc \
+    init.sensor_1_0.rc \
+    init.mmi.chipset.rc \
+    apanic.rc \
+    init.mmi.rc \
+    init.ago.rc \
+    init.mt6785.rc \
+    meta_init.project.rc \
+    init.mmi.tcmd.rc \
+    factory_init.connectivity.rc \
+    init.mmi.overlay.rc \
+    init.connectivity.rc \
+    init_connectivity.rc \
+    factory_init.connectivity.common.rc \
+    meta_init.modem.rc \
+    meta_init.connectivity.common.rc \
+    init.mmi.backup.trustlet.rc \
+    factory_init.project.rc \
+    init.mt6785.usb.rc \
+    init.aee.rc \
+    init.recovery.mt6785.rc \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6785:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6785
+
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
